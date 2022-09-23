a9fc537d97e66ba1196cb912891176d75ea475ae
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-09-22 20:35:56 -0500

    Corrections

--- histogram.py
+++ histogram.py
@@ -6,46 +6,51 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import utils
+import argparse
 
 PATH_TO_DATASET = 'dataSet/DJI_'
 PATH_TO_OUTPUT = 'cropImages/'
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, type = int, help = "# of image")
+ap.add_argument("-f", "--fragment", required = True, type = int, help = "# of fragment")
+args = vars(ap.parse_args())
 
-def histogram(args):
-    image = cv2.imread(PATH_TO_OUTPUT + 'image'+str(args['image'])+'/fragment'+str(args['fragment'])+'.jpg')
-    cv2.imshow("image", image)
+
+image = cv2.imread(PATH_TO_OUTPUT + 'image'+str(args['image'])+'/fragment'+str(args['fragment'])+'.jpg')
+cv2.imshow("image", image)
 
 
-    colorsRep = {}
-    for i in range(len(image)):
-        for j in range(len(image[i])):
-            hexa = utils.rgb_to_hex(image[i][j][0], image[i][j][1], image[i][j][2])
-            if hexa in colorsRep.keys():
-                colorsRep[hexa] += 1
-            else:
-                colorsRep[hexa] = 1
+colorsRep = {}
+for i in range(len(image)):
+    for j in range(len(image[i])):
+        hexa = utils.rgb_to_hex(image[i][j][0], image[i][j][1], image[i][j][2])
+        if hexa in colorsRep.keys():
+            colorsRep[hexa] += 1
+        else:
+            colorsRep[hexa] = 1
 
-    l = list(colorsRep.values())
-    for i in range(len(l)):
-        for j in range(i + 1, len(l)):
+l = list(colorsRep.values())
+for i in range(len(l)):
+    for j in range(i + 1, len(l)):
 
-            if l[i] < l[j]:
-                l[i], l[j] = l[j], l[i]
+        if l[i] < l[j]:
+            l[i], l[j] = l[j], l[i]
 
-    highers = l[:20]
-    colorsFrag = []
-    for key, value in colorsRep.items():
-        for higher in highers:
-            if value == higher:
-                colorsFrag.append(key)
-                break
+highers = l[:20]
+colorsFrag = []
+for key, value in colorsRep.items():
+    for higher in highers:
+        if value == higher:
+            colorsFrag.append(key)
+            break
 
-    ax = plt.axes()
-    ax.set_facecolor("black")
-    plt.figure("Histogram in fragment")
-    plt.xlabel("Colors")
-    plt.ylabel("Repetitions")
-    plt.title("Predominant colors in fragment")
+ax = plt.axes()
+ax.set_facecolor("black")
+plt.figure("Histogram in fragment")
+plt.xlabel("Colors")
+plt.ylabel("Repetitions")
+plt.title("Predominant colors in fragment")
 
-    for i in range(len(colorsFrag)):
-        plt.bar(colorsFrag[i], highers[i], 
-                color = [colorsFrag[i]])
+for i in range(len(colorsFrag)):
+    plt.bar(colorsFrag[i], highers[i], 
+            color = [colorsFrag[i]])
--- preprocesing.py
+++ preprocesing.py
@@ -2,41 +2,46 @@ from sklearn.cluster import KMeans
 import utils
 import cv2
 import numpy as np
+import argparse
 
 PATH_TO_DATASET = 'dataSet/DJI_'
 PATH_TO_OUTPUT = 'cropImages/'
 
-# Argumnet for number of clusters K
-def preprocesing(args):
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--clusters", required = True, type = int, help = "# of clusters")
+args = vars(ap.parse_args())
 
-    # load the image and convert it from BGR to RGB so that
-    # we can dispaly it with matplotlib
-    image = cv2.imread(PATH_TO_DATASET+'0145.TIF')
-    cv2.imshow("image", image)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+# load the image and convert it from BGR to RGB so that
+# we can dispaly it with matplotlib
+image = cv2.imread(PATH_TO_OUTPUT+'image1/fragment48.jpg')
+cv2.imshow("image", image)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    a = np.asarray(image,dtype=np.float32)/255
-    h, w, c = a.shape
+a = np.asarray(image,dtype=np.float32)/255
+h, w, c = a.shape
 
-    # reshape the image to be a list of pixels
-    image = a.reshape((image.shape[0] * image.shape[1], 3))
+# reshape the image to be a list of pixels
+image = a.reshape((image.shape[0] * image.shape[1], 3))
 
-    # cluster the pixel intensities
-    clt = KMeans(n_clusters = args["clusters"])
-    clt.fit(image)
+# cluster the pixel intensities
+clt = KMeans(n_clusters = args["clusters"])
+clt.fit(image)
 
-    # build a histogram of clusters and then create a figure
-    # representing the number of pixels labeled to each color
+# build a histogram of clusters and then create a figure
+# representing the number of pixels labeled to each color
 
-    centroids = clt.cluster_centers_
-    labels = clt.labels_
+centroids = clt.cluster_centers_
+labels = clt.labels_
 
-    a2k = centroids[labels]
-    a3k = a2k.reshape(h, w, c)
+a2k = centroids[labels]
+a3k = a2k.reshape(h, w, c)
 
+kmeansImage = cv2.cvtColor(a3k, cv2.COLOR_RGB2BGR)
+cv2.imshow("a3k", kmeansImage)
 
-    kmeansImage = cv2.cvtColor(a3k, cv2.COLOR_RGB2BGR)
-    cv2.imshow("a3k", kmeansImage)
+hist = utils.centroid_histogram(clt)
+colors = utils.plot_colors(hist, clt.cluster_centers_, hexa=True)
 
-    hist = utils.centroid_histogram(clt)
-    utils.plot_colors(hist, clt.cluster_centers_, hexa=True)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
